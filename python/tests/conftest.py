634b9e75ab2b19fd516db6c42ba00daae84edd37
Sameer Khan <94801110+khansameer25@users.noreply.github.com>
2025-05-21 19:55:14 -0400

    Update conftest.py

--- python/tests/conftest.py
+++ python/tests/conftest.py
@@ -1,8 +1,13 @@
+from _pytest.nodes import Item
 # this supposedly shortens the long traceback
 def pytest_exception_interact(node, call, report):
+    if not isinstance(node, Item):
+        return
+        
     excinfo = call.excinfo
-    if 'example' in node.funcargs:
-        excinfo.traceback.cut(path=node.funcargs['example'])
+    example_path = getattr(node, "funcargs", {}).get("example")
+    if example_path:
+        excinfo.traceback.cut(path=example_path)
     report.longrepr = node.repr_failure(excinfo)
 
 import matplotlib
