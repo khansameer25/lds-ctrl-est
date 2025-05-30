f24f3e893437b63d08cd9fe1d12f681199cc741a
Sameer Khan <94801110+khansameer25@users.noreply.github.com>
2025-05-29 22:20:27 -0400

    Update setup.py

--- python/setup.py
+++ python/setup.py
@@ -1,15 +1,10 @@
-from setuptools import setup, find_packages
+from setuptools import setup
+from setuptools.dist import Distribution
+
+class BinaryDistribution(Distribution):
+    def has_ext_modules(self):
+        return True
 
 setup(
-    name="ldsctrlest",
-    version="0.9.0",
-    description="Python bindings for ldsCtrlEst",
-    author="Kyle Johnsen, Michael Bolus",
-    packages=find_packages(),
-    include_package_data=True,
-    package_data={
-        "ldsctrlest": ["*.so", "*.pyd", "*.dll", "*.dylib", "*.lib", "*.pdb"],
-    },
-    python_requires=">=3.6",
-    license="Apache-2.0",
+    distclass=BinaryDistribution,
 )
